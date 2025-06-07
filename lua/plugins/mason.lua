58d6a547e44cf25af3ca29648fdb0eebf512da8d
sush-il <zoroat3@gmail.com>
2025-06-07 16:56:16 +0100

    LSP setup

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        'mason-org/mason.nvim',
+        config = function()
+            require('mason').setup()
+        end,
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = { 'lua_ls', 'ts_ls' }
+        },
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    }
+}
