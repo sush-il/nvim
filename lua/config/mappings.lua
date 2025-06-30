03b3def258c7d85ffb01cd4262b87abb184acfee
sush-il <zoroat3@gmail.com>
2025-06-30 21:30:47 +0100

    feat: add mappings moving / duplicating line

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,4 +1,4 @@
--- ### This file only contains core mappings ### --
+-- ### This file only contains core mappins ### --
 -- ### For plugin mappings check the file with the plugin name ### --
 -- ### Some cases instead of plugin configs are grouped, e.g. 'theming' ### --
 
@@ -12,16 +12,13 @@ map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "Copy entire file to clipboard" })
 map("n", "<leader>n", "<cmd>set nu!<CR>",   { desc = "Toggle line numbers" })
 map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle relative line numbers" })
 
-
--- INSERT MODE 
+-- INSERT MODE  
 
 map("i", "<C-b>", "<ESC>^i", { desc = "Move to beginning of line" })
-map("i", "<C-e>", "<End>", { desc = "Move to end of line" })
-map("i", "<C-h>", "<Left>", { desc = "Move left" })
+map("i", "<C-e>", "<End>",   { desc = "Move to end of line" })
+map("i", "<C-h>", "<Left>",  { desc = "Move left" })
 map("i", "<C-l>", "<Right>", { desc = "Move right" })
-map("i", "<C-j>", "<Down>", { desc = "Move down" })
-map("i", "<C-k>", "<Up>", { desc = "Move up" })
-
+map("i", "<C-h>", "<C-w>",   { desc = "Remove word backwards" })
 
 -- NORMAL MODE 
 
@@ -30,6 +27,10 @@ map("n", "d", '"_d', { desc = "Delete motion)" })
 map("n", "D", '"_D', { desc = "Delete to EOL" })
 map("n", "x", '"_x', { desc = "Delete character" })
 
+map("n", "<M-j>", "ddp", { desc = "Move line down" })
+map("n", "<M-k>", "ddkP", { desc = "Move line up" })
+map("n", "<M-S-j>", "yyp", { desc = "Duplicate line down" })
+map("n", "<M-S-k>", "yyP", { desc = "Duplicate line up" })
 
 -- VISUAL MODE 
 
@@ -37,6 +38,9 @@ map("v", "d", '"_d', { desc = "Delete selection" })
 
 
 -- WINDOW NAVIGATION 
+
+map("n", "<leader>sr", ":vsplit<CR>", { desc = "Split window to right" })
+map("n", "<Leader>sd", ":split<CR>",  { desc = "Split window below" })
 
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
